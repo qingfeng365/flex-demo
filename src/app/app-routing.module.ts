b3ec41cbbf6afeb5fdd66570dd150d059d4d2cf8
qingfengwuyou <h3658686@126.com>
2017-11-09 09:54:03 +0800

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BasicComponent } from './basic/basic.component';
+import { ColumnComponent } from './column/column.component';
+import { DiceComponent } from './dice/dice.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  redirectTo: 'basic',
+  pathMatch: 'full'
+},
+{
+  path: 'basic',
+  component: BasicComponent
+},
+{
+  path: 'column',
+  component: ColumnComponent
+},
+{
+  path: 'dice',
+  component: DiceComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasicComponent } from './basic/basic.component';
+import { ColumnComponent } from './column/column.component';
+import { DiceComponent } from './dice/dice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicComponent,
+    ColumnComponent,
+    DiceComponent
   ],
   imports: [
     BrowserModule,
